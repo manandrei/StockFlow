d025f8add32f592adaaf12a7c402770585fa538c
Andrei-Catalin Man <man.andrei@softunity.ro>
2023-10-30 02:18:14 +0200

    Remove unnecessary comments and downgrade package version
    All superfluous comments have been removed from the `Weather.razor` and `Program.cs` files to make the code cleaner. Additionally, the package `Microsoft.EntityFrameworkCore.Design` has been downgraded from version `8.0.0-rc.2.23480.1` to `8.0.0-rc.1.23419.6` in the `StockFlow.Web.csproj` file.

--- src/StockFlow.Web/StockFlow.Web/Program.cs
+++ src/StockFlow.Web/StockFlow.Web/Program.cs
@@ -4,7 +4,6 @@ using StockFlow.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
@@ -12,7 +11,6 @@ builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("De
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -20,7 +18,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 

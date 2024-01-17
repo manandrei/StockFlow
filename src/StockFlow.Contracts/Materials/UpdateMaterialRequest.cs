9a76e082b0350be25dbfd305752111976c5dcc34
Andrei-Catalin Man <man.andrei@softunity.ro>
2024-01-17 13:23:27 +0200

    Refactor material request models
    Removed constructors from the `AddMaterialRequest` and `UpdateMaterialRequest` models in the material services of the StockFlow.Contracts project. Updated `PartNumber` and `SizeType` properties with the `required` keyword to ensure these fields are always provided. These changes simplify the models and reinforce essential data requirements.

--- src/StockFlow.Contracts/Materials/UpdateMaterialRequest.cs
+++ src/StockFlow.Contracts/Materials/UpdateMaterialRequest.cs
@@ -4,23 +4,15 @@ namespace StockFlow.Contracts.Materials;
 
 public record UpdateMaterialRequest
 {
-    public UpdateMaterialRequest(long Id, string PartNumber, string SizeType, string Description)
-    {
-        this.Id = Id;
-        this.PartNumber = PartNumber;
-        this.SizeType = SizeType;
-        this.Description = Description;
-    }
-
     [Required(ErrorMessage = "Id is required")]
     public long Id { get; init; }
 
     [Required, MaxLength(7, ErrorMessage = "Part Number cannot be longer than 7 characters"), MinLength(3, ErrorMessage = "Part Number cannot be shorter than 3 characters")]
-    public string PartNumber { get; init; }
+    public required string PartNumber { get; init; }
 
     [Required]
     //Todo: convert to enum and make it required and convertible to the domain enum type (StockFlow.Domain.Materials.SizeType)
-    public string SizeType { get; init; }
+    public required string SizeType { get; init; }
 
     public string? Description { get; init; }
 }
